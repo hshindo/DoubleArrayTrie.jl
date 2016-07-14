2efb01ebbdf6e483d0268598f0b47956dc23a6b5
Hiroyuki Shindo <hshindo@outlook.com>
2016-07-14 09:49:53 +0900

    Update sandbag

--- test/sandbag.jl
+++ test/sandbag.jl
@@ -10,6 +10,8 @@ words = map(chomp, lines)
 keys = map(w -> [Int(c) for c in w], words)
 trie = DATrie(keys, words)
 
+get(trie, [35], "")
+
 for i = 1:length(words)
   v = get(trie, keys[i], "")
   @assert v == words[i]
